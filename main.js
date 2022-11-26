984ef777e68fcdafe3197b59ce1ea28fa7ba4bc6
AntonyJr00 <antonyrodl20@gmail.com>
2022-11-26 11:44:44 -0500

    avanze 26/11/2022

--- main.js
+++ main.js
@@ -1,24 +1,23 @@
 ///ejercicio 10y 11------------
 
-// let users = [];
-// let idUse = 0;
-
-// function informacion() {
-//   const info = prompt("ingrese su: nombre, apellido, edad, profesion");
-//   let dato = info.split(", ");
-//   //   let fecha = new Date();
-//   let objeto = {
-//     id: idUse,
-//     nombre: dato[0],
-//     apellido: dato[1],
-//     edad: dato[2],
-//     profesion: dato[3],
-//     fechaCreada: new Date(año, mes, fecha),
-//   };
-//   idUse++;
-//   users.push(objeto);
-//   console.log(objeto);
-// }
+let users = [];
+let idUse = 0;
+function informacion() {
+  const info = prompt("ingrese su: nombre, apellido, edad, profesion");
+  let dato = info.split(", ");
+  let objeto = {
+    id: idUse,
+    nombre: dato[0],
+    apellido: dato[1],
+    edad: dato[2],
+    profesion: dato[3],
+    //EJERCICIO 11
+    fechaCreada: new Date(),
+  };
+  idUse++;
+  users.push(objeto);
+  console.log(objeto);
+}
 
 // informacion();
 
@@ -88,37 +87,26 @@ const usuarios = [
 // fechaOrdenada(usuarios);
 
 //13. Crear una funcion que permita filtrar los usuarios por mes y año de creacion.
-function filtrarfecha(usuarios) {
-  let datedilter = usuarios.filter((a) => a.created_at === usuarios);
-  let result = datedilter.length === 0 ? "fecha encontrada" : datedilter;
-  console.log(result);
-} // no resuelta xd, va a ser subida al git
+// function filtrarfecha(mes = "", año = "") {
+//   let datedilter = usuarios.filter((item) => {
+//     const getMes = item.created_at.toLocaleDateString().split("/")[1];
+//     const getAnio = item.created_at.toLocaleDateString().split("/")[2];
+//     if (getMes == mes || getAnio == año) {
+//       return true;
+//     }
+//     return false;
+//   });
+//   let result = !datedilter.length ? "fecha no encontrada" : datedilter;
+//   console.log(result);
+// }
+// let mes = prompt("ingrese mes: ");
+// let año = prompt("ingrese año: ");
+// filtrarfecha(mes, año);
 
-//10. Crear una funcion que te permita ingresar en un prompt la informacion de un usuario de la siguiente manera:
-// "Ingrese la informacion del usuario (nombre, apellido, edad, profesion)"
-// El usuario digitara: Andres, Perez, 28, ingeniero.
-// La informacion debe ser guardada como objeto dentro del array users asignadole un id unico a cada registro.
-
-//11. Utilizando el objeto Date, añadir la propiedad created_date de manera interna en donde se registre el
-// momento en que ese registro fue creado.
-
-//12.
-
-// let users = [
-//     {id: 1, nombre: "Andres", apellido: "Pacheco", edad: 38, profesion: "developer", created_at: "2022-09-26T06:25:21.118Z"},
-//     {id: 2, nombre: "Andrea", apellido: "Sanchez", edad: 25, profesion: "profesor", created_at: "2022-04-18T14:14:32.879Z"},
-//     {id: 3, nombre: "Julia", apellido: "Ochoa", edad: 32, profesion: "musico", created_at: "2021-12-14T11:53:38.279Z"},
-//     {id: 4, nombre: "Samuel", apellido: "Martinez", edad: 29, profesion: "programador", created_at: "2022-01-26T03:31:15.202Z"},
-//     {id: 5, nombre: "Roberto", apellido: "Mattos", edad: 40, profesion: "chef", created_at: "2022-07-27T02:06:22.760Z"},
-//     {id: 6, nombre: "Mercedes", apellido: "Sanchez", edad: 35, profesion: "veterinario", created_at: "2022-05-01T22:06:35.864Z"},
-// ]
-
-// Crear una funcion que permita ordenar la lista de usuarios por fecha de creacion, de la mas nueva a la mas antigua y viceversa
-// utilizando el parametro booleano reverse (si es true se ordenaran de nuevo a antiguo)
-
-//13. Crear una funcion que permita filtrar los usuarios por mes y año de creacion.
-
-//14. Elaborar un programa que permita al admin a traves de prompts y alerts lo siguiente:
+// 14" Elaborar un programa que permita al admin a traves de prompts y alerts lo siguiente:"
+function sistema() {
+  //en proceso
+}
 // CREATE
 // El admin debe poder crear un nuevo registro de usuario utilizando la funcion 10.
 
